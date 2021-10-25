2960fb34e8d546fccecad25e229813b741acc593
sh.khalajestanii <NiH@sh123>
2021-10-25 14:48:24 +0330

    Added Jwt access and refresh tokens.

--- src/main/java/com/shahintraining/authenticationservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/shahintraining/authenticationservice/filter/CustomAuthenticationFilter.java
@@ -1,10 +1,16 @@
 package com.shahintraining.authenticationservice.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.shahintraining.authenticationservice.domain.AppUser;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +18,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 /**
  * @author sh.khalajestanii
@@ -44,7 +52,21 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        User user= (User) authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("long ass secret key".getBytes());
+        String access_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withIssuedAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withIssuer(request.getRequestURI())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withIssuedAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withIssuer(request.getRequestURI())
+                .sign(algorithm);
+        response.setHeader("access_token",access_token);
+        response.setHeader("refersh_token",refresh_token);
     }
 
 }
